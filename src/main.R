b85c324afd86316ed5d33b9205dfb750fddda7fa
ricco-budget-lab <john.ricco@yale.edu>
2023-10-02 16:17:38 -0400

    Add basic structure

---
+++ src/main.R
@@ -0,0 +1,26 @@
+#---------------------------------------------
+# main.R
+# 
+# Entry point into Tax-Data processing module
+#---------------------------------------------
+
+
+
+# Load required packages
+lapply(readLines('requirements.txt'), library, character.only = T)
+
+
+# TODO steps
+
+# 1) read and process PUF
+
+# 2) read and process targets
+
+# 3) create 2017 PUF
+
+# 4) create 2019 targets for censored vars
+
+# 5) create 2019 PUF 
+
+# 6) create 2020 -> 2053 PUFs
+
