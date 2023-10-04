78c445c6adf9367e49fc6cdd1de9f29e86d7ae62
ricco-budget-lab <john.ricco@yale.edu>
2023-10-04 17:49:49 -0400

    Finish compiling SOI tables

--- src/process_targets.R
+++ src/process_targets.R
@@ -4,8 +4,16 @@
 # TODO
 #------------------------------------
 
+
 # Read tables 
-table_names = c('table_1_6', 'table_2_3') 
+table_names = c('table_1_4', 
+                'table_1_4_kg', 
+                'table_1_6', 
+                'table_2_1', 
+                'table_2_3', 
+                'table_3_3',
+                'line_item_estimates')
+
 tables = table_names %>% 
   map(.f = ~ file.path(interface_paths$`Compiled-SOI-Tables`, paste0(.x, '.csv')) %>% 
                read_csv()) %>% 
@@ -16,23 +24,59 @@ tables = table_names %>%
 # Process tables
 #----------------
 
+
+# Table 1.4: reshape long in variable, wide in series type
+tables$table_1_4 %<>% 
+  pivot_longer(cols      = contains('__'), 
+               names_to  = c('series', 'variable'), 
+               names_sep = '__', 
+               values_to = 'value') %>% 
+  pivot_wider(names_from  = series, 
+              values_from = value)
+
+# Table 1.4 - capital gains: reshape long in variable, wide in series type
+tables$table_1_4_kg %<>% 
+  pivot_longer(cols      = contains('__'), 
+               names_to  = c('series', 'variable'), 
+               names_sep = '__', 
+               values_to = 'value') %>% 
+  pivot_wider(names_from  = series, 
+              values_from = value)
+
 # Table 1.6: reshape long in agi group
 tables$table_1_6 %<>% 
   pivot_longer(cols      = -c(year, filing_status, age_group), 
                names_to  = 'agi', 
-               values_to = 'value') %>% 
+               values_to = 'count') %>% 
   mutate(variable = 'returns', 
          agi      = as.numeric(agi)) %>% 
   relocate(year, variable)
 
+# Table 2.1: reshape long in variable, wide in series type
+tables$table_2_1 %<>% 
+  pivot_longer(cols      = contains('__'), 
+               names_to  = c('series', 'variable'), 
+               names_sep = '__', 
+               values_to = 'value') %>% 
+  pivot_wider(names_from  = series, 
+              values_from = value)
 
 # Table 2.3: reshape long in variable 
 tables$table_2_3 %<>%
   pivot_longer(cols      = contains('dep'), 
                names_to  = 'variable', 
-               values_to = 'value') %>% 
+               values_to = 'count') %>% 
   relocate(year, variable) %>%
   arrange(year, variable)
+
+# Table 3.3: reshape long in variable, wide in series type
+tables$table_3_3 %<>% 
+  pivot_longer(cols      = contains('__'), 
+               names_to  = c('series', 'variable'), 
+               names_sep = '__', 
+               values_to = 'value') %>% 
+  pivot_wider(names_from  = series, 
+              values_from = value)
 
 
 
@@ -68,12 +112,13 @@ get_target_value = function(constraint) {
   
   # Get target value and return
   this_table %>% 
-    filter(variable == constraint$variable, 
+    filter(year == constraint$year,
+           variable == constraint$variable, 
            filing_status %in% constraint$filing_status, 
            age_group %in% constraint$age_group, 
            agi >= constraint$agi_min, 
            agi < constraint$agi_max) %>% 
-    summarise(target_value = sum(value)) %>% 
+    summarise(target_value = sum(count)) %>% 
     return()
 }
 
