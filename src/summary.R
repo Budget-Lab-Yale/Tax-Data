0213c95344250ba8a8b912c47094368030c3c532
kendall-budget-lab <joshua.m.kendall@yale.edu>
2023-10-11 12:40:55 -0400

    updated summary functions

--- src/summary.R
+++ src/summary.R
@@ -2,29 +2,44 @@
 
 reweight_summary = function(puf, rw, vars) {
   
+  
   target_summary = get_target_summary(vars)
   
+  counts = puf %>%
+    mutate(across(weight, ~ .x * rw)) %>%
+    group_by(agi_group) %>%
+    summarise(across(all_of(vars), ~ sum((.x > 0) * weight))) %>%
+    pivot_longer(!agi_group,
+                 names_to= "Variable",
+                 values_to = "PUF_Count")
+    
   puf %>%
     mutate(across(weight, ~ .x * rw)) %>%
     group_by(agi_group) %>%
-    summarise(across(vars, ~ sum(.x * weight))) %>%
+    summarise(across(all_of(vars), ~ sum(.x * weight))) %>%
     pivot_longer(!agi_group,
                  names_to= "Variable",
                  values_to = "PUF_Amount") %>%
+    left_join(counts, by = c('agi_group', 'Variable')) %>%
     left_join(target_summary, by = c('agi_group', "Variable")) %>%
-    filter(!is.na(SOI_Amount)) %>%
-    mutate(gap = SOI_Amount - PUF_Amount) %>%
-    select(Variable, agi_group, SOI_Amount, PUF_Amount, gap)  %>%
+    mutate(Amount_Gap = SOI_Amount - PUF_Amount,
+           Amount_pct_off = Amount_Gap * 100 / SOI_Amount,
+           Count_Gap = SOI_Count - PUF_Count,
+           Count_pct_off = Count_Gap * 100 / SOI_Count) %>%
+    select(Variable, agi_group, 
+           SOI_Amount, PUF_Amount, SOI_Count, PUF_Count, 
+           Amount_Gap, Amount_pct_off, Count_Gap, Count_pct_off)  %>%
     return()
 }
 
 
 get_target_summary = function(vars) {
   
+  
   guide = read_csv("resources/variable_guide.csv")
   
-  target_summary = data.frame(matrix(ncol = 3, nrow = 1))
-  colnames(target_summary) = c("agi", "variable", "amount")
+  target_summary = data.frame(matrix(ncol = 4, nrow = 1))
+  colnames(target_summary) = c("agi", "variable", "count", "amount")
   
   for(i in vars) {
     this_var = filter(guide, variable==i)
@@ -34,7 +49,7 @@ get_target_summary = function(vars) {
       
       target_summary %<>%
         bind_rows(select(filter(this_table, variable==this_var$variable),
-                         agi, variable, amount))
+                         agi, variable, count, amount))
     }
   }
   
@@ -43,6 +58,7 @@ get_target_summary = function(vars) {
   target_summary %>%
     rename(Variable = variable,
            SOI_Amount = amount,
+           SOI_Count = count,
            agi_group = agi) %>%
     return()
 }
