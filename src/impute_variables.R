ae63b999279f5acf8764df00607384195a9ad011
ricco-budget-lab <john.ricco@yale.edu>
2024-01-29 15:30:11 -0500

    Start new projections process

--- src/impute_variables.R
+++ src/impute_variables.R
@@ -5,14 +5,11 @@
 #--------------------------------------
 
 # TODO list for the future
-# - model 65+ 
-# - get actual distributions of ages within buckets
-# - improve secondary age imputation
-# - improve earnings split imputation
+# - improve depedent age imputation
 # - vary SSN status by income
 # - actually impute pretax contributions
 # - higher fidelty wage earnings split for EARNINGS var, including consistent notion of gender/primary status interaction  
-
+# - gender based on Saez-Zucman 2016 tabulations
 
 
 
@@ -35,24 +32,77 @@ tax_units %<>%
 # Primary and secondary ages
 #----------------------------
 
-tax_units %<>% 
-  mutate(
-    
-    # Distribute ages uniformly within bands; top-code ages at 65
-    age1 = case_when(
-      age_group == 1 ~ floor(runif(nrow(.), 18, 26)), 
-      age_group == 2 ~ floor(runif(nrow(.), 26, 35)),
-      age_group == 3 ~ floor(runif(nrow(.), 35, 45)), 
-      age_group == 4 ~ floor(runif(nrow(.), 45, 55)),
-      age_group == 5 ~ floor(runif(nrow(.), 55, 65)),
-      T              ~ 65
-    ),
-    
-    # Set secondary age to primary age for joint returns
-    age2 = if_else(filing_status == 2, age1, NA)
-  ) %>% 
+# Read the CPS
+cps = interface_paths$`CPS-ASEC` %>% 
+  file.path('cps_00013.csv.gz') %>% 
+  read_csv()
+
+# Estimate age gap distribution 
+age_gap_dist = cps %>% 
+  filter(RELATE %in% c(0101, 0201), CPSID != 0, AGE >= 18) %>% 
+  mutate(role = if_else(RELATE == 0101, 'primary', 'secondary')) %>% 
+  select(YEAR, CPSID, role, age = AGE, weight = ASECWTH ) %>%
+  mutate(age = if_else(age >= 80, 80, age)) %>%
+  pivot_wider(names_from  = role, 
+              values_from = age) %>% 
+  group_by(age1 = primary, age2 = secondary) %>% 
+  filter(!is.na(age2)) %>%
+  summarise(n = sum(weight), 
+            .groups = 'drop') %>% 
+  arrange(age1, age2) %>% 
+  group_by(age1) %>% 
+  mutate(p = n / sum(n)) %>% 
+  ungroup() %>% 
+  select(-n)
   
-  # Clean up 
+# Estimate distribution of ages within PUF age categories
+cps_ages = cps %>%
+  mutate(age = if_else(AGE >= 80, 80, AGE)) %>% 
+  group_by(age) %>% 
+  summarise(n = sum(ASECWT)) 
+
+age_dist = cps_ages %>% 
+  mutate(age = if_else(age >= 80, 80, age)) %>% 
+  filter(age >= 18) %>%
+  group_by(age_group = case_when(
+    age <= 26 ~ 1, 
+    age <= 35 ~ 2, 
+    age <= 45 ~ 3,
+    age <= 55 ~ 4, 
+    age <= 65 ~ 5,
+    T         ~ 6
+  )) %>% 
+  mutate(p = n / sum(n)) %>% 
+  select(-n) 
+  
+
+# Impute ages 
+imputed_ages = tax_units %>% 
+  select(id, age_group) %>% 
+  left_join(age_dist, 
+            by = 'age_group', 
+            relationship = 'many-to-many') %>% 
+  group_by(id) %>% 
+  sample_n(size = 1, weight = p) %>% 
+  ungroup() %>% 
+  select(id, age1 = age)
+
+# Impute secondary ages 
+imputed_secondary_ages = tax_units %>% 
+  left_join(imputed_ages, by = 'id') %>% 
+  filter(filing_status == 2) %>% 
+  select(id, age1) %>% 
+  left_join(age_gap_dist, 
+            by = 'age1', 
+            relationship = 'many-to-many') %>% 
+  group_by(id) %>% 
+  sample_n(size = 1, weight = p) %>% 
+  ungroup() %>% 
+  select(id, age2)
+
+tax_units %<>%
+  left_join(imputed_ages, by = 'id') %>% 
+  left_join(imputed_secondary_ages, by = 'id') %>% 
   select(-age_group)
 
 
@@ -70,7 +120,7 @@ dep_ages = tax_units %>%
       dep_age_group1 == 3 ~ floor(runif(nrow(.), 13, 17)), 
       dep_age_group1 == 4 ~ floor(runif(nrow(.), 17, 19)),
       dep_age_group1 == 5 ~ floor(runif(nrow(.), 19, 24)),
-      dep_age_group1 == 6 ~ floor(runif(nrow(.), 24, 95)),
+      dep_age_group1 == 6 ~ pmin(80, floor(runif(nrow(.), 24, 95))),
       T                   ~ NA
     ),
     dep_age2 = case_when(
@@ -79,7 +129,7 @@ dep_ages = tax_units %>%
       dep_age_group2 == 3 ~ floor(runif(nrow(.), 13, 17)), 
       dep_age_group2 == 4 ~ floor(runif(nrow(.), 17, 19)),
       dep_age_group2 == 5 ~ floor(runif(nrow(.), 19, 24)),
-      dep_age_group2 == 6 ~ floor(runif(nrow(.), 24, 95)),
+      dep_age_group2 == 6 ~ pmin(80, floor(runif(nrow(.), 24, 95))),
       T                   ~ NA
     ),
     dep_age3 = case_when(
@@ -88,7 +138,7 @@ dep_ages = tax_units %>%
       dep_age_group3 == 3 ~ floor(runif(nrow(.), 13, 17)), 
       dep_age_group3 == 4 ~ floor(runif(nrow(.), 17, 19)),
       dep_age_group3 == 5 ~ floor(runif(nrow(.), 19, 24)),
-      dep_age_group3 == 6 ~ floor(runif(nrow(.), 24, 95)),
+      dep_age_group3 == 6 ~ pmin(80, floor(runif(nrow(.), 24, 95))),
       T                   ~ NA
     )
     
@@ -147,13 +197,90 @@ tax_units %<>%
                             runif(nrow(.)) > share_17yo_ineligible, 
                             dep_ctc3))
 
+#-------------------
+# Gender for adults
+#-------------------
+
+# Estimate target male distribution parameters for unmarried tax units on DINA data
+male_dist = dina_2017 %>% 
+  filter(married == 0) %>% 
+  group_by(filer, has_kids = xkidspop > 0, employed = fiwag > 0, female) %>% 
+  summarise(n = sum(dweght), 
+            .groups = 'drop') %>% 
+  group_by(filer, has_kids, employed) %>% 
+  mutate(p_male = 1 - n / sum(n)) %>%
+  filter(female == 1) %>% 
+  select(-n, -female) %>% 
+  ungroup()
+
+# Back out imputation probabilities by targeting DINA male shares
+male_dist_impute = tax_units %>% 
+  mutate(sex = as.integer(if_else(filer == 0, NA, GENDER == 1))) %>% 
+  filter(filing_status != 2) %>%
+  group_by(filer, has_kids = n_dep_ctc > 0, employed = wages > 0) %>% 
+  summarise(male     = sum((!is.na(sex) & sex == 1) * weight), 
+            missing  = sum(is.na(sex)), 
+            n_weight = sum(weight), 
+            n        = n(),
+            .groups = 'drop') %>%
+  mutate(p_male_reported = male / n_weight, 
+         p_reported      = 1 - missing / n) %>% 
+  select(-male, -missing, -n) %>% 
+  
+  left_join(male_dist, by = c('filer', 'has_kids', 'employed')) %>% 
+  mutate(p_male_impute = pmin(1, pmax(0, (p_male - p_male_reported * p_reported) / (1 - p_reported))))
+  
+
+# Impute in data 
+tax_units %<>% 
+  
+  # Join unmarried imputation probabilities
+  mutate(has_kids = n_dep_ctc > 0, employed = wages > 0) %>%
+  left_join(male_dist_impute %>% 
+              select(filer, has_kids, employed, p_male_impute), 
+            by = c('filer', 'has_kids', 'employed')) %>% 
+  mutate(
+    
+    # Choose randomly for all nonfilers
+    GENDER = if_else(filer == 0, NA, GENDER),
+    
+    # Primary earner
+    male1 = case_when(
+      
+      # Married and missing: choose randomly
+      filing_status == 2 & is.na(GENDER) ~ as.integer(runif(nrow(.)) < 0.5),
+      
+      # Married and nonmissing: take from PUF
+      filing_status == 2 & GENDER == 1 ~ 1, 
+      filing_status == 2 & GENDER == 2 ~ 0, 
+      
+      # Unmarried and missing: simulate  
+      filing_status != 2 & is.na(GENDER) ~ runif(nrow(.)) < p_male_impute,
+      
+      # Unmarried and nonmissing: take from PUF
+      filing_status != 2 & GENDER == 1 ~ 1, 
+      filing_status != 2 & GENDER == 2 ~ 0
+    ), 
+    
+    # Secondary earner...assume 1% of marrages are same-sex  
+    male2 = case_when(
+      filing_status == 2 & male1 == 1 ~ if_else(runif(nrow(.)) < 0.01, 1, 0), 
+      filing_status == 2 & male1 == 0 ~ if_else(runif(nrow(.)) < 0.01, 0, 1), 
+      TRUE                            ~ NA
+    )
+  ) %>% 
+  
+  select(-p_male_impute, -has_kids, -employed)
+  
+
+
 
 #--------------------------------
 # Impute SSN status for children
 #--------------------------------
 
 # Source: ITEP (https://itep.org/inclusive-child-tax-credit-reform-would-restore-benefit-to-1-million-young-dreamers/)
-share_without_ssn = 1098100 / 75e6
+share_without_ssn = 1098100 / 72e6
 
 # Assume perfect correlation of SSN status within tax units
 tax_units %<>% 
@@ -278,7 +405,7 @@ sstb_params = qbi_params %>%
   pivot_wider(names_from   = sstb, 
               names_prefix = 'sstb', 
               values_from  = n) %>% 
-  mutate(p_sstb = (sstb1 / (sstb0 + sstb1)) - 0.05) %>%  # Add factor calibrated to match QBI totals  
+  mutate(p_sstb = (sstb1 / (sstb0 + sstb1)) - 0.1) %>%  # Add factor calibrated to match QBI totals  
   select(form, p_sstb)
 
 # Assumption: wages paid are shared out in proportion to:
