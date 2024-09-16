feb982f14c0874c4b349ea803aefeef1e3ca08e1
ricco-budget-lab <john.ricco@yale.edu>
2024-09-16 16:17:41 -0400

    Add FLSA overtime imputation

--- src/impute_variables.R
+++ src/impute_variables.R
@@ -912,6 +912,123 @@ tax_units %<>%
   )
 
 
+#---------
+# FLSA OT
+#---------
+
+# Read and clean data
+ot_microdata = haven::read_dta('./resources/otdata2023.dta') %>% 
+  mutate(
+    wages    = total_weekly_earnings * wkswork1, 
+    ot       = ot_weekly_earnings * wkswork1 * (flsa_eligible > 0),
+    has_ot   = as.integer(ot > 0),
+    ot_share = ot / wages, 
+    married  = married == 1, 
+    parent   = numkids > 0, 
+    age      = as.numeric(age)
+  ) %>%
+  filter(wages > 0, !is.na(ot), age >= 18) %>% 
+  mutate(
+    wage_pctile = cut(
+      x      = wages, 
+      breaks = wtd.quantile(wages + runif(nrow(.)), weight, 0:100 / 100), 
+      labels = 1:100
+    ) %>% as.character() %>% as.numeric()
+  ) %>% 
+  filter(!is.na(wage_pctile)) 
+
+
+# Estimate model of presence of FLSA-eligible OT  
+has_ot_qrf = quantregForest(
+  x        = ot_microdata[c('wage_pctile', 'parent', 'married', 'age')],
+  y        = as.factor(ot_microdata$has_ot), 
+  nthreads = parallel::detectCores(),
+  weights  = ot_microdata$annualweight,
+  mtry     = 3,
+  nodesize = 1
+)
+
+# Estimate model of presence of FLSA-eligible OT  
+has_ot_microdata = ot_microdata %>% 
+  filter(ot > 0)
+ot_share_qrf = quantregForest(
+  x        = has_ot_microdata[c('wage_pctile', 'parent', 'married', 'age')],
+  y        = has_ot_microdata$ot_share, 
+  nthreads = parallel::detectCores(),
+  weights  = has_ot_microdata$annualweight,
+  mtry     = 3,
+  nodesize = 5
+)
+
+
+# Fit values on tax data
+max_ot = max(ot_microdata$ot)
+actual_p = ot_microdata %>% 
+  summarise(weighted.mean(has_ot, annualweight)) %>% 
+  deframe()
+ot = tax_units %>% 
+  filter(wages1 > 0 | wages2 > 0) %>% 
+  mutate(
+    parent = (
+      (!is.na(dep_age1) & dep_age1 <= 24) | 
+        (!is.na(dep_age2) & dep_age2 <= 24) | 
+        (!is.na(dep_age3) & dep_age3 <= 24)
+    ), 
+    married = married == 1
+  ) %>% 
+  select(id, weight, parent, married, wages1, wages2, age1, age2) %>% 
+  pivot_longer(
+    cols            = c(wages1, wages2), 
+    names_prefix    = 'wages', 
+    names_transform = as.integer, 
+    names_to        = 'index', 
+    values_to       = 'wages'
+  ) %>% 
+  mutate(age = if_else(index == 1, age1, age2)) %>% 
+  select(-age1, -age2) %>% 
+  filter(wages > 0) %>% 
+  mutate(
+    wage_pctile = cut(
+      x      = wages, 
+      breaks = wtd.quantile(wages + runif(nrow(.)), weight, 0:100 / 100), 
+      labels = 1:100
+    ) %>% as.character() %>% as.numeric() %>% replace_na(1)
+  ) %>% 
+  mutate(
+    p = predict(
+      object  = has_ot_qrf, 
+      newdata = (.),
+      what    = function(x) mean(x - 1)
+    ),
+    p  = p * actual_p / weighted.mean(p, weight),
+    ot = pmin(max_ot, predict(
+      object  = ot_share_qrf, 
+      newdata = (.),
+      what    = function(x) sample(x, 1)
+    ) * wages * (runif(nrow(.)) < p))
+  )
+
+
+# Add OT-related info to tax unit data
+tax_units %<>% 
+  left_join(
+    ot %>% 
+      select(id, index, ot) %>% 
+      pivot_wider(
+        names_from   = index,
+        names_prefix = 'ot',
+        values_from  = ot,
+      ), 
+    by = 'id'
+  ) %>% 
+  mutate(
+    ot1    = replace_na(ot1, 0), 
+    ot2    = replace_na(ot2, 0), 
+    ot     = ot1 + ot2
+  )
+
+
+
 #-----------
 # TODO LIST
 #-----------
